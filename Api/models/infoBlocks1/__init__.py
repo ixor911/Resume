ea513b031ff36d557ab2ca249ed58baa31b4f35c
ixor911 <igorbutenko9111@gmail.com>
2025-02-08 12:08:28 +0100

    models outdated deleted

--- Api/models/infoBlocks1/__init__.py
+++
@@ -1,5 +0,0 @@
-from .ObjectsBlock import ObjectsBlock, ObjectsBlockSerializer
-from .DetailsBlock import DetailsBlock, DetailsBlockSerializer
-from .FilesBlock import FilesBlock, FilesBlockSerializer
-
-from . import adds
--- Api/models/infoBlocks1/adds/__init__.py
+++
@@ -1,6 +0,0 @@
-from .Education import Education, EducationSerializer
-from .File import File, FileSerializer
-from .LanguageScale import LanguageScale, LanguageScaleSerializer
-from .Link import Link, LinkSerializer
-from .ProfessionalExperience import ProfessionalExperience, ProfessionalExperienceSerializer
-from .SkillScale import Skill, SkillScaleSerializer
